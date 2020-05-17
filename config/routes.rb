e47c02f9554cab181eed9dc65d342dd55f36f7e7
ktozawa <ubuntu@ip-172-31-43-116.ap-northeast-1.compute.internal>
2020-05-17 11:51:45 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # root 'application#hello'
   get 'static_pages/home'
   # => StaticPages#home
   get 'static_pages/help'
   # => # => StaticPages#home
-  root 'application#hello'
+  get 'static_pages/about'
+  # => # => StaticPages#home
+ 
 end
