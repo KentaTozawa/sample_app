383d1be5d81d5defeb7b7c9bf3fde0e6bbdb13a6
ktozawa <ubuntu@ip-172-31-43-116.ap-northeast-1.compute.internal>
2020-05-17 10:04:57 +0000

    Static_pages homehelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+  # => StaticPages#home
+  get 'static_pages/help'
+  # => # => StaticPages#home
   root 'application#hello'
 end
