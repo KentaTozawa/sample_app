62aae2a2b2956588e785f0a3b77c8ca1befc04dd
ktozawa <ubuntu@ip-172-31-43-116.ap-northeast-1.compute.internal>
2020-05-29 10:27:34 +0000

    fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  #root 'static_pages#home'
   # root 'application#hello'
-  get 'static_pages/home'
+  #get 'static_pages/home'
   # => StaticPages#home
-  get 'static_pages/help'
+  #get 'static_pages/help'
   # => # => StaticPages#home
-  get 'static_pages/about'
+  #get 'static_pages/about'
   # => # => StaticPages#home
-  get 'static_pages/contact'
- 
+  #get 'static_pages/contact'
+  
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
